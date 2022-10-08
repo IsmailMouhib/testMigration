452d35d09d83052fc2aa585ed991b9897474c3e9
ismail_mouhib <ismailmouhib@gmail.com>
2022-10-08 20:54:09 +0100

    commit 3 new repo

--- exposition/src/main/java/com/example/maritimeassurance/MaritimeAssuranceApplication.java
+++ exposition/src/main/java/com/example/maritimeassurance/MaritimeAssuranceApplication.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class MaritimeAssuranceApplication{
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(MaritimeAssuranceApplication.class, args);
 	}
 
